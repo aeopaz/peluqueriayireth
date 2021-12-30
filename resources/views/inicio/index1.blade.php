79718245e1ada295594832277341e3f8e9d9a4dc
aocampo <aocampo@shivam.com.co>
2021-12-30 09:17:33 -0500

    Agregar secure_asset

--- resources/views/inicio/index1.blade.php
+++ resources/views/inicio/index1.blade.php
@@ -25,13 +25,13 @@
     <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img class="d-block w-100" src="{{ asset('img/fondo1.gif') }}" alt="First slide">
+                    <img class="d-block w-100" src="{{ secure_asset('img/fondo1.gif') }}" alt="First slide">
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="{{ asset('img/fondo2.jpg') }}" alt="Second slide">
+                    <img class="d-block w-100" src="{{ secure_asset('img/fondo2.jpg') }}" alt="Second slide">
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="{{ asset('img/fondo3.jpg') }}" alt="Third slide">
+                    <img class="d-block w-100" src="{{ secure_asset('img/fondo3.jpg') }}" alt="Third slide">
                 </div>
             </div>
         </div>
